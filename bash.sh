d0b8b3c901dfc3ff3a85557b4b50d7fbabc5c85b
loyejaotdiqr47123 <102934154+loyejaotdiqr47123@users.noreply.github.com>
2024-01-07 05:09:22 +0000

    Update

--- bash.sh
+++ bash.sh
@@ -4,5 +4,5 @@ URL=${REPL_SLUG}.${REPL_OWNER}.repl.co
 
 echo -e "Replit 保活日志：\e[0m"
 
-while true; do curl -s 'https://acgapi.loyejaotdiqr471.repl.co' --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36" >/dev/null 2>&1 && echo "$(date +'%Y%m%d%H%M%S') 请求成功 ..." && sleep 1; done &
+while true; do curl -s 'https://35932bc2-d342-4ddc-8d8f-718cb3f8eb9c-00-16ourpq2b2y16.riker.replit.dev/' --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36" >/dev/null 2>&1 && echo "$(date +'%Y%m%d%H%M%S') 请求成功 ..." && sleep 1; done &
 python3 index.py
